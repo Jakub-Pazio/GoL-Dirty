068140b15367448e7835bcc8fd7092b4dd66478c
Jakub-Pazio <19jakubpazio99@gmail.com>
2024-07-06 16:18:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+var Cell;
+(function (Cell) {
+    Cell[Cell["Dead"] = 0] = "Dead";
+    Cell[Cell["Alive"] = 1] = "Alive";
+})(Cell || (Cell = {}));
+;
+;
+class GameImpl {
+    constructor(size) {
+        this.board = Array.from({ length: size }, () => Array(size).fill(Cell.Dead));
+    }
+    ;
+    cellNeighNumber(x, y) {
+        const directions = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1]
+        ];
+        return directions
+            .map(([dx, dy]) => [x + dx, y + dy])
+            .filter(([nx, ny]) => nx >= 0 && nx < this.board.length && ny >= 0 && ny < this.board[0].length)
+            .reduce((count, [nx, ny]) => count + (this.board[nx][ny] === Cell.Alive ? 1 : 0), 0);
+    }
+    cellNextState(x, y) {
+        const neighs = this.cellNeighNumber(x, y);
+        let res;
+        switch (this.board[x][y]) {
+            case Cell.Dead:
+                res = neighs === 3 ? Cell.Alive : Cell.Dead;
+                break;
+            case Cell.Alive:
+                res = (neighs === 2) || (neighs === 3) ? Cell.Alive : Cell.Dead;
+                break;
+        }
+        return res;
+    }
+    nextGame() {
+        let res = new GameImpl(100);
+        for (const { x, y } of this.iterateBoard()) {
+            res.board[x][y] = this.cellNextState(x, y);
+        }
+        return res;
+    }
+    *iterateBoard() {
+        for (let x = 0; x < this.board.length; x++) {
+            for (let y = 0; y < this.board[x].length; y++) {
+                yield { cell: this.board[x][y], x, y };
+            }
+        }
+    }
+}
+let game1 = new GameImpl(100);
+game1.board[2][1] = Cell.Alive;
+game1.board[2][2] = Cell.Alive;
+game1.board[2][3] = Cell.Alive;
+game1.board[0][2] = Cell.Alive;
+game1.board[1][3] = Cell.Alive;
+let game2 = game1.nextGame();
+// for (const {cell, x, y} of game1.iterateBoard()) {
+//     game2.board[x][y] = cell;
+// }
+console.log(game1);
+console.log(game2);
+const rootDOM = document.getElementById('root');
+const ncButton = document.getElementById('newCycleButton');
+const renderGame = () => {
+    rootDOM.innerHTML = "";
+    for (let x = 0; x < 100; x++) {
+        for (let y = 0; y < 100; y++) {
+            let square = document.createElement('div');
+            square.id = `square${x}${y}`;
+            square.style.width = "20px";
+            square.style.height = "20px";
+            square.style.display = "inline-block";
+            square.style.textAlign = "center";
+            square.style.verticalAlign = "top";
+            square.style.border = "1px solid black";
+            square.style.backgroundColor = game1.board[x][y] === Cell.Alive ? "black" : "white";
+            rootDOM.appendChild(square);
+        }
+        rootDOM.appendChild(document.createElement('br'));
+    }
+};
+const renderNextFrame = () => {
+    for (let x = 0; x < 100; x++) {
+        for (let y = 0; y < 100; y++) {
+            const square = document.getElementById(`square${x}${y}`);
+            square.style.backgroundColor = game1.board[x][y] === Cell.Alive ? "black" : "white";
+        }
+    }
+};
+ncButton.addEventListener('click', () => {
+    renderNextFrame();
+    game1 = game1.nextGame();
+});
+renderGame();
